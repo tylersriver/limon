294c84ad51bb1ca47a0d8c1aa88a6f7e1d66d9f8
tylersriver <tyler.w.sriver@eagles.oc.edu>
2021-10-15 11:13:50 -0500

    Fix new function

--- src/functions.php
+++ src/functions.php
@@ -125,13 +125,14 @@ if (!function_exists('Yocto\render')) {
 }
 
 if (!function_exists('getallheaders')) {
-    function getallheaders() {
-    $headers = [];
-    foreach ($_SERVER as $name => $value) {
-        if (substr($name, 0, 5) == 'HTTP_') {
-            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+    function getallheaders()
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
         }
-    }
-    return $headers;
+        return $headers;
     }
 }
