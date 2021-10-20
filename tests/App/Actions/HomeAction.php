b04101b304d8acae1b33717969e88976e6a7032a
tylersriver <tyler.w.sriver@eagles.oc.edu>
2021-10-20 15:21:52 -0500

    Add view test with injected param

--- tests/App/Actions/HomeAction.php
+++ tests/App/Actions/HomeAction.php
@@ -3,6 +3,8 @@
 namespace Yocto\Tests\App\Actions;
 
 use Yocto\Action;
+use Yocto\Attributes\Parameter;
+use Yocto\Attributes\Required;
 use Yocto\Response;
 
 use function Yocto\render;
@@ -10,8 +12,13 @@ use function Yocto\html;
 
 class HomeAction extends Action
 {
+
+    #[Parameter('title', Parameter::GET, '^(\w|\s)+$')]
+    #[Required]
+    protected string $title;
+
     public function action(): Response
     {
-        return html(render('index'));
+        return html(render('home', ['title' => $this->title]));
     }
 }
